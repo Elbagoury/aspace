2038dcb3273045fa3b1bd5014dcdd2e025c8decf
Carrysoft <carrysoft@gmail.com>
2018-11-28 20:41:46 +0100

    Primer envio

---
+++ oehealth_es/__openerp__.py
@@ -0,0 +1,39 @@
+##############################################################################
+#    Copyright (C) 2017 oeHealth (<http://oehealth.in>). All Rights Reserved
+#    oeHealth, Hospital Management Solutions
+##############################################################################
+
+{
+    'name': 'oeHealth Spanish Translation',
+    'version': '1.0',
+    'author': "Braincrew Apps",
+    'category': 'Generic Modules/Medical',
+    'summary': 'Translate the complete oeHealth module in Spanish Language',
+    'depends': ['base'],
+    'price': 50.00,
+    'currency': 'EUR',
+    'description': """
+
+oeHealth Spanish Traslation
+
+""",
+    "images": ['images/main_screenshot.png'],
+    "website": "http://oehealth.in",
+    "data": [
+    ],
+    "demo": [
+
+    ],
+    'test':[
+    ],
+    'css': [
+
+    ],
+    'js': [
+
+    ],
+    'qweb': [
+
+    ],
+    "active": False
+}
